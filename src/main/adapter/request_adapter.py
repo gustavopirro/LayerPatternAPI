555f5c207a20c635036ec31c4c3a28f5382e901e
gustavo.pirro <gustavo.pirro@gmail.com>
2023-03-06 20:57:37 -0300

    feat - added request adapter

---
+++ src/main/adapter/request_adapter.py
@@ -0,0 +1,16 @@
+from typing import Type
+from flask import request as FlaskRequest
+from src.view.http_types.http_request import HttpRequest
+from src.view.http_types.http_response import HttpResponse
+from src.view.interfaces.views_interface import ViewInterface
+
+def request_adapter(request: FlaskRequest, callback: Type[ViewInterface]) -> HttpResponse:
+    http_request = HttpRequest(
+        header=request.headers,
+        body=request.json,
+        query_params=request.args,
+        url=request.full_path,
+    )
+
+    http_response = callback.handle(http_request)
+    return http_response
