15cc80eb2bad32117ec8e325004dd6ea02e8c24a
gustavo.pirro <gustavo.pirro@gmail.com>
2023-03-09 22:29:04 -0300

    config - Created data base and SQL struct file

---
+++ SQL_DATA.sql
@@ -0,0 +1,36 @@
+-- --------------------------------------------------------
+-- Servidor:                     C:\Users\gusta\Desktop\Projetos\LayerPatternAPI\project.db
+-- Versão do servidor:           3.39.4
+-- OS do Servidor:               
+-- HeidiSQL Versão:              12.4.0.6659
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES  */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para project
+CREATE DATABASE IF NOT EXISTS "project";
+;
+
+-- Copiando estrutura para tabela project.persons
+CREATE TABLE IF NOT EXISTS "persons" (
+	"name" VARCHAR(50) NOT NULL DEFAULT NULL,
+	"age" INTEGER NOT NULL DEFAULT 0,
+	"address" VARCHAR(50) NOT NULL DEFAULT '0',
+	"profession" VARCHAR(50) NOT NULL DEFAULT '0',
+	PRIMARY KEY ("name")
+);
+
+-- Exportação de dados foi desmarcado.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
